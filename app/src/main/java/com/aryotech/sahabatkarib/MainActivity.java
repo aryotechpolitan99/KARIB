fe5a0c9cf7c88284b5dfd5df937370289867e026
aryotech <aryofreeyoung@gmail.com>
2020-07-07 17:07:22 +0700

    bottom navigation with fragment

--- app/src/main/java/com/aryotech/sahabatkarib/MainActivity.java
+++ app/src/main/java/com/aryotech/sahabatkarib/MainActivity.java
@@ -4,17 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.aryotech.sahabatkarib.OptionActivity.BoosterActivity;
-import com.aryotech.sahabatkarib.OptionActivity.EventActivity;
-import com.aryotech.sahabatkarib.OptionActivity.ShoopingActivity;
+import com.aryotech.sahabatkarib.Fragment.Booster;
+import com.aryotech.sahabatkarib.Fragment.Event;
+import com.aryotech.sahabatkarib.Fragment.Shoping;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,35 +25,66 @@ public class MainActivity extends AppCompatActivity {
         //Initialize and Assign Variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
 
-        //Set Event Selected
-        bottomNavigationView.setSelectedItemId(R.id.event);
-
-        //Perform ItemSelectedListener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                switch (menuItem.getItemId()) {
-                    case R.id.event:
-                        startActivity(new Intent(getApplicationContext()
-                                , EventActivity.class));
-                        overridePendingTransition(0, 0);
-                        finish();
-                        return true;
-                    case R.id.booster:
-                        startActivity(new Intent(getApplicationContext()
-                                , BoosterActivity.class));
-                        finish();
-                        return true;
-                    case R.id.shop:
-                        startActivity(new Intent(getApplicationContext()
-                                , ShoopingActivity.class));
-                        overridePendingTransition(0, 0);
-                        finish();
-                        return true;
-                }
-                return false;
-            }
-        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new Event()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    Fragment fragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.event:
+                            fragment = new Event();
+                            break;
+
+                        case R.id.booster:
+                            fragment = new Booster();
+                            break;
+
+                        case R.id.shop:
+                            fragment = new Shoping();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+                    return false;
+                }
+            };
+
+
+//        //Set Event Selected
+//        bottomNavigationView.setSelectedItemId(R.id.event);
+//
+//        //Perform ItemSelectedListener
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//
+//                switch (menuItem.getItemId()) {
+//                    case R.id.event:
+//                        startActivity(new Intent(getApplicationContext()
+//                                , EventActivity.class));
+//                        overridePendingTransition(0, 0);
+//                        finish();
+//                        return true;
+//                    case R.id.booster:
+//                        startActivity(new Intent(getApplicationContext()
+//                                , BoosterActivity.class));
+//                        finish();
+//                        return true;
+//                    case R.id.shop:
+//                        startActivity(new Intent(getApplicationContext()
+//                                , ShoopingActivity.class));
+//                        overridePendingTransition(0, 0);
+//                        finish();
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
+
 }
