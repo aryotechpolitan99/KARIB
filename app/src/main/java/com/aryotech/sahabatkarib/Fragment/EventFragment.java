ee0ae4f2737cb1d547a531fa600f79a22460cbce
aryotech <aryofreeyoung@gmail.com>
2020-07-14 13:28:00 +0700

    EventFargment = fecthing data from firebase into recyclerview

--- app/src/main/java/com/aryotech/sahabatkarib/Fragment/EventFragment.java
+++ app/src/main/java/com/aryotech/sahabatkarib/Fragment/EventFragment.java
@@ -2,19 +2,37 @@ package com.aryotech.sahabatkarib.Fragment;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aryotech.sahabatkarib.Model.Event;
+import com.aryotech.sahabatkarib.Model.ViewHolder;
 import com.aryotech.sahabatkarib.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EventFragment extends Fragment {
+
+    private View EventsView;
+    LinearLayoutManager eLinearLayoutManager;
+    RecyclerView eRecyclerView;
+    FirebaseDatabase eFirebaseDatabase;
+    DatabaseReference eDatabaseReference;
+    FirebaseRecyclerAdapter<Event, ViewHolder> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Event> option;
 
 
     public EventFragment() {
@@ -26,7 +44,68 @@ public class EventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        EventsView = inflater.inflate(R.layout.fragment_event, container, false);
+
+        eLinearLayoutManager = new LinearLayoutManager(getContext());
+        eLinearLayoutManager.setReverseLayout(true);
+        eLinearLayoutManager.setStackFromEnd(true);
+
+        eRecyclerView = EventsView.findViewById(R.id.recyclerView);
+
+        eFirebaseDatabase = FirebaseDatabase.getInstance();
+        eDatabaseReference = eFirebaseDatabase.getReference("Data");
+
+        showData();
+
+        return EventsView;
+    }
+
+    private void showData(){
+        option = new FirebaseRecyclerOptions.Builder<Event>().setQuery(eDatabaseReference, Event.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Event, ViewHolder>(option) {
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_list, parent, false);
+                ViewHolder viewHolder = new ViewHolder(itemView);
+                viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+
+
+
+                    }
+                });
+                return viewHolder;
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder holder, int i, @NonNull Event event) {
+
+                holder.setDetails(getContext(),event.getImage(), event.getTittle());
+
+            }
+        };
+
+        eRecyclerView.setLayoutManager(eLinearLayoutManager);
+        firebaseRecyclerAdapter.startListening();
+        eRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public void onStart(){
+
+        super.onStart();
+
+        if (firebaseRecyclerAdapter != null){
+            firebaseRecyclerAdapter.startListening();
+        }
     }
 
 }
--- app/src/main/java/com/aryotech/sahabatkarib/MainActivity.java
+++ app/src/main/java/com/aryotech/sahabatkarib/MainActivity.java
@@ -1,39 +1,21 @@
 package com.aryotech.sahabatkarib;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.aryotech.sahabatkarib.Fragment.BoosterFragment;
 import com.aryotech.sahabatkarib.Fragment.EventFragment;
 import com.aryotech.sahabatkarib.Fragment.ShopingFragment;
-import com.aryotech.sahabatkarib.Model.Event;
-import com.aryotech.sahabatkarib.Model.ViewHolder;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private BottomNavigationView bottomNavigationView;
-    LinearLayoutManager eLinearLayoutManager;
-    RecyclerView eRecyclerView;
-    FirebaseDatabase eFirebaseDatabase;
-    DatabaseReference eDatabaseReference;
-    FirebaseRecyclerAdapter<Event, ViewHolder> firebaseRecyclerAdapter;
-    FirebaseRecyclerOptions<Event> option;
 
 
     @Override
@@ -41,72 +23,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        eLinearLayoutManager = new LinearLayoutManager(this);
-        eLinearLayoutManager.setReverseLayout(true);
-        eLinearLayoutManager.setStackFromEnd(true);
-
-        eRecyclerView = findViewById(R.id.recyclerView);
-        eFirebaseDatabase = FirebaseDatabase.getInstance();
-        eDatabaseReference = eFirebaseDatabase.getReference("Data");
-
-        showData();
 
         //Initialize and Assign Variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new EventFragment()).commit();
-    }
-
-    private void showData() {
-
-        option = new FirebaseRecyclerOptions.Builder<Event>().setQuery(eDatabaseReference, Event.class).build();
-
-        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Event, ViewHolder>(option){
-            @Override
-            protected void onBindViewHolder(@NonNull ViewHolder holder, int i, @NonNull Event event) {
-
-                holder.setDetails(getApplicationContext(), event.getImage(), event.getTittle());
-
-            }
-
-            @NonNull
-            @Override
-            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_list, parent, false);
-                ViewHolder viewHolder = new ViewHolder(itemView);
-                viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-
-                        Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onItemLongClick(View view, int position) {
-
-                        Toast.makeText(MainActivity.this, "Long Click", Toast.LENGTH_SHORT).show();
-
-                    }
-
-                });
-                return viewHolder;
-            }
-        };
-
-        eRecyclerView.setLayoutManager(eLinearLayoutManager);
-        firebaseRecyclerAdapter.startListening();
-        eRecyclerView.setAdapter(firebaseRecyclerAdapter);
-    }
-
-
-    protected void onStart() {
-        super.onStart();
-
-        if (firebaseRecyclerAdapter != null) {
-            firebaseRecyclerAdapter.startListening();
-        }
     }
 
     BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
